97ecb2a082548d241f6216bb4f63799023388a2c
Ksenia Lohanko <99492970+KsenaLokhanko@users.noreply.github.com>
2023-10-02 22:51:23 +0300

    Add files via upload

---
+++ lab4/Cosmetic.java
@@ -0,0 +1,76 @@
+package lab4;
+
+/**
+ * The `Cosmetic` class represents a cosmetic product with properties like product type, brand, shade, price,
+ * and whether it is hypoallergenic.
+ */
+public class Cosmetic {
+    private String productType;
+    private String brand;
+    private String shade;
+    private double price;
+    private boolean isHypoallergenic;
+
+    /**
+     * Constructs a new `Cosmetic` object with the specified properties.
+     *
+     * @param productType     The type of cosmetic product (e.g., lipstick, foundation).
+     * @param brand           The brand of the cosmetic product.
+     * @param shade           The shade or color of the cosmetic product.
+     * @param price           The price of the cosmetic product.
+     * @param isHypoallergenic Whether the cosmetic product is hypoallergenic or not.
+     */
+    public Cosmetic(String productType, String brand, String shade, double price, boolean isHypoallergenic) {
+        this.productType = productType;
+        this.brand = brand;
+        this.shade = shade;
+        this.price = price;
+        this.isHypoallergenic = isHypoallergenic;
+    }
+
+    /**
+     * Gets the product type of the cosmetic.
+     *
+     * @return The product type.
+     */
+    public String getProductType() {
+        return productType;
+    }
+
+    /**
+     * Gets the brand of the cosmetic.
+     *
+     * @return The brand.
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * Gets the shade or color of the cosmetic.
+     *
+     * @return The shade.
+     */
+    public String getShade() {
+        return shade;
+    }
+
+    /**
+     * Gets the price of the cosmetic.
+     *
+     * @return The price.
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * Checks whether the cosmetic is hypoallergenic or not.
+     *
+     * @return True if the cosmetic is hypoallergenic, false otherwise.
+     */
+    public boolean getIsHypoallergenic() {
+        return isHypoallergenic;
+    }
+}
+
---
+++ lab4/Main.java
@@ -0,0 +1,66 @@
+package lab4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * This class demonstrates printing an array of objects of the `Cosmetic` class and sorting it.
+ */
+public class Main {
+
+    /**
+     * The main method of this program
+     */
+    public static void main(String[] args) {
+        // Defining the task variant
+        int N = 1117;
+        System.out.printf("C11=%d;\n", N % 11);
+        System.out.println("Визначити клас косметика, який складається як мінімум з 5-и полів.");
+
+        // Create an array of Cosmetic objects
+        Cosmetic[] cosmeticProducts = new Cosmetic[]{
+                new Cosmetic("Lipstick", "LANCOME", "Red", 8.99, false),
+                new Cosmetic("Foundation", "GlamBee", "Beige", 19.99, true),
+                new Cosmetic("Mascara", "MAYBELLINE", "Black", 10.59, true),
+                new Cosmetic("Eyeshadow", "LANCOME", "Blue", 14.99, false)
+        };
+
+        // Check for exceptions
+        for (Cosmetic object : cosmeticProducts) {
+            if (object.getPrice() < 0) {
+                System.err.format("%.2f$ - prise for %s %s is negative, it must be at least more than zero.",
+                        object.getPrice(),
+                        object.getProductType(),
+                        object.getBrand());
+                return;
+            }
+        }
+        System.out.println("\nNot sorted:");
+        printArr(cosmeticProducts);
+        // Sort by ascending price (using Comparator)
+        Arrays.sort(cosmeticProducts, Comparator.comparingDouble(Cosmetic::getPrice));
+        System.out.println("\nSorted by prise:");
+        printArr(cosmeticProducts);
+        // Sort by name in reverse (using Comparator)
+        Arrays.sort(cosmeticProducts, Comparator.comparing(Cosmetic::getProductType).reversed());
+        System.out.println("\nSorted by abc revers:");
+        printArr(cosmeticProducts);
+    }
+
+    /**
+     * Method for outputting an array.
+     * The output text is formatted.
+     *
+     * @param array an array to be printed.
+     */
+    private static void printArr(Cosmetic[] array) {
+        for (Cosmetic object : array) {
+            System.out.format("%s %s, %s, prise: %.2f$, is hypoallergenic: %b\n",
+                    object.getProductType(),
+                    object.getBrand(),
+                    object.getShade(),
+                    object.getPrice(),
+                    object.getIsHypoallergenic());
+        }
+    }
+}
